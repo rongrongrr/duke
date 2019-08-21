f7d21a12ba5b1a456ea46de8eef91d9ba260ed6d
rongrongrr <jingrong.contact@gmail.com>
2019-08-21 23:33:05 +0800

    Add arraylist to store tasks. Return appropriate output based on user input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String logo = " ____        _        \n"
@@ -15,14 +17,23 @@ public class Duke {
         System.out.println("    ____________________________________________________________");
         System.out.println();
 
-        String command = scanner.next();
+        String command = scanner.nextLine();
+        ArrayList<String> list = new ArrayList<>();
 
         while(!command.equals("bye")) {
             System.out.println("    ____________________________________________________________");
-            System.out.println("     " + command);
+            if (!command.equals("list")) {
+                list.add(command);
+                System.out.println("     added: " + command);
+            } else {
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println(String.format("     %d. %s", i + 1, list.get(i)));
+                }
+            }
+
             System.out.println("    ____________________________________________________________");
             System.out.println();
-            command = scanner.next();
+            command = scanner.nextLine();
         }
 
         System.out.println("    ____________________________________________________________");
