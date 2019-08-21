3672239c245256f4f7e5d51248537542590f13d6
rongrongrr <jingrong.contact@gmail.com>
2019-08-21 23:16:31 +0800

    Add scanner, lines and indentations. Echo commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,32 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     Hello! I'm Duke\n     What can I do for you?");
+        System.out.println("    ____________________________________________________________");
+        System.out.println();
+
+        String command = scanner.next();
+
+        while(!command.equals("bye")) {
+            System.out.println("    ____________________________________________________________");
+            System.out.println("     " + command);
+            System.out.println("    ____________________________________________________________");
+            System.out.println();
+            command = scanner.next();
+        }
+
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     Bye. Hope to see you again soon!");
+        System.out.println("    ____________________________________________________________");
     }
 }
