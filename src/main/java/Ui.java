a6cb27bbac34aea1423b3a7dfa064d6876b5bd1b
rongrongrr <jingrong.contact@gmail.com>
2019-09-19 16:07:16 +0800

    Finalise features

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -10,6 +10,8 @@ public class Ui {
     private Parser parser;
     private StringBuilder string;
     private TaskList undoneTasks;
+    private int meowCount;
+    private int purrCount;
 
     /**
      * Creates a Ui object tagged with scanner, whether the program is exited, and parser.
@@ -19,13 +21,15 @@ public class Ui {
         isExit = false;
         parser = new Parser();
         undoneTasks = new TaskList();
+        meowCount = 0;
+        purrCount = 0;
     }
 
     /**
      * Displays a welcome message.
      */
     public String showWelcome() {
-        return "Hello! I'm Duke\nWhat can I do for you?";
+        return "Meow! I'm Duke :3\nWhat can I do for you nya?";
     }
 
     /**
@@ -58,13 +62,40 @@ public class Ui {
             isExit = true;
             break;
         case "list":
-            string.append("Here are the tasks in your list:\n");
+            string.append("Here are the tasks in your list nya:\n");
             for (int i = 0; i < tasks.getTasks().size(); i++) {
                 string.append(String.format("     %d.%s\n", i + 1, tasks.getTasks().get(i).toString()));
             }
             break;
-        case "undo": //need to reload old file
-            string.append("Noted. Your previous command has been undone. Here are the tasks in your list:\n");
+        case "meow":
+            meowCount += 1;
+            if (meowCount >= 3) {
+                string.append("Nya!!! Give me a useful command!! >:3");
+                meowCount = 0;
+            } else {
+                for (int i = 0; i < meowCount; i++) {
+                    string.append("Meow~~~ ");
+                }
+            }
+            break;
+        case "pet":
+            purrCount += 1;
+            if (purrCount == 3) {
+                string.append("Nya!!! Stop petting or I will scratch you *hiss* >:3");
+            } else if (purrCount > 3) {
+                string.append("HISS!!! *scratch* *angry cat noises*");
+                purrCount = 0;
+            } else {
+                for (int i = 0; i < purrCount; i++) {
+                    string.append("Purr~~~ ");
+                }
+            }
+            break;
+        case "":
+            string.append("Meow? Please say something!");
+            break;
+        case "undo":
+            string.append("Meow! Noted~ Your previous command has been undone. Here are the tasks in your list nya:\n");
             while(!tasks.getTasks().isEmpty()) {
                 tasks.delete(0);
             }
@@ -82,25 +113,33 @@ public class Ui {
             int index;
             switch (com) {
             case "delete":
-                string.append("Noted. I've removed this task:\n");
                 index = Integer.valueOf(commands[1]) - 1;
-                Task deletedTask = tasks.getTasks().get(index);
-                string.append("       " + deletedTask.toString() + "\n");
-                tasks.delete(index);
-                if (!tasks.getTasks().isEmpty()) {
-                    Task checkDeletedTask = tasks.getTasks().get(index);
-                    assert !deletedTask.toString().equals(checkDeletedTask.toString()) :
-                            "the task at this index should have been deleted";
+                if (tasks.getTasks().isEmpty()) {
+                    string.append("Nya? There is nothing to remove nya *confused cat noises*");
+                } else if (index >= tasks.getTasks().size()) {
+                    string.append(String.format("Nya? There are no tasks at (%d) nya", index + 1));
+                } else {
+                    string.append("Meow! Noted~ I've removed this task nya:\n");
+                    Task deletedTask = tasks.getTasks().get(index);
+                    string.append("       " + deletedTask.toString() + "\n");
+                    tasks.delete(index);
+                    boolean isMatching = false;
+                    for (Task task : tasks.getTasks()) {
+                        if (deletedTask.equals(task)) {
+                            isMatching = true;
+                        }
+                    }
+                    assert isMatching : "the task at this index should have been deleted nya";
+                    string.append(String.format("Now you have %d tasks in the list nya~\n", tasks.getTasks().size()));
                 }
-                string.append(String.format("Now you have %d tasks in the list.\n", tasks.getTasks().size()));
                 break;
             case "done":
                 index = Integer.valueOf(commands[1]) - 1;
                 Task t = tasks.getTasks().get(index);
                 tasks.done(index);
                 String isDone = t.isDone();
-                assert isDone.equals("y") : "the task should be marked done";
-                string.append("Nice! I've marked this task as done:\n");
+                assert isDone.equals("y") : "the task should be marked done nya";
+                string.append("Nice nya! I've marked this task as done nya:\n");
                 string.append("       " + t.toString() + "\n");
                 break;
             case "find":
@@ -110,7 +149,7 @@ public class Ui {
                         results.add(task);
                     }
                 }
-                string.append("Here are the matching tasks in your list:\n");
+                string.append("Here are the matching tasks in your list nya:\n");
                 for (int i = 0; i < results.size(); i++) {
                     string.append(String.format("     %d.%s\n", i + 1, results.get(i).toString()));
                 }
@@ -129,9 +168,9 @@ public class Ui {
                 if (commands[0].equals("todo") || commands[0].equals("deadline") || commands[0].equals("event")) {
                     if (commands.length == 1) {
                         if (commands[0].equals("event")) {
-                            string.append("OOPS!!! The description of an event cannot be empty.\n");
+                            string.append("OOPS!!! The description of an event cannot be empty nya.\n");
                         } else {
-                            string.append(String.format("OOPS!!! The description of a %s cannot be empty.\n",
+                            string.append(String.format("OOPS!!! The description of a %s cannot be empty nya.\n",
                                     commands[0]));
                         }
 
@@ -151,23 +190,23 @@ public class Ui {
 
                         int taskSize = tasks.getTasks().size();
                         tasks.add(task);
-                        string.append("Got it. I've added this task:\n");
+                        string.append("Meow! Got it~ I've added this task nya:\n");
                         string.append("       " + task.toString() + "\n");
                         if (tasks.getTasks().size() == 1) {
-                            string.append("Now you have 1 task in the list.\n");
+                            string.append("Now you have 1 task in the list nya~\n");
 
                         } else {
-                            string.append(String.format("Now you have %d tasks in the list.\n", tasks.getTasks()
+                            string.append(String.format("Now you have %d tasks in the list nya~\n", tasks.getTasks()
                                     .size()));
 
                         }
 
                         int newTaskSize = tasks.getTasks().size();
-                        assert newTaskSize == taskSize + 1 : "the list should have increased by 1 task";
+                        assert newTaskSize == taskSize + 1 : "the list should have increased by 1 task nya";
                     }
 
                 } else {
-                    string.append("OOPS!!! I'm sorry, but I don't know what that means :-(\n");
+                    string.append("OOPS!!! I'm sorry, but I don't know what that means nya :,3\n");
                 }
             }
         }
@@ -202,6 +241,6 @@ public class Ui {
      * Shows exit message for stopping program.
      */
     public String exit() {
-        return "Bye. Hope to see you again soon!\n";
+        return "Bye bye! Hope to see you again soon nya! :3 :3 :3\n";
     }
 }
